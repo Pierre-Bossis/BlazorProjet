ab27b8fd203b30ebb90a9b6ae6b6a8a3426d123c
Pierre-Bossis <pbossis@hotmail.com>
2022-08-21 10:49:31 +0200

    first commit

---
+++ BlazorApp1/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BlazorApp1.Models
+{
+    [Serializable]
+    public class Contact
+    {
+        public string firstName { get; set; }
+
+        public string lastName { get; set; }
+
+        public string email { get; set; }
+    }
+}
